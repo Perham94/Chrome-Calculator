a5468e9082abb422a18fcea49e171291287b2427
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2018-11-10 19:45:58 +0100

    YO

--- popup.js
+++ popup.js
@@ -5,10 +5,10 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  let color = element.target.value;
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'document.body.style.backgroundColor = "' + color + '";'
     });
-  };
+  });
+};
